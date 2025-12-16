46d4d12647d39c4d448355f6a8f0e79221caca48
Andrew Yong <email.xryong@gmail.com>
2025-12-16 09:51:52 +0800

    refactor: remove unused import of StructureBlockEntity in StructureBlockRenderer_mixin for cleaner code

--- src/main/java/carpet/mixins/StructureBlockRenderer_mixin.java
+++ src/main/java/carpet/mixins/StructureBlockRenderer_mixin.java
@@ -2,7 +2,6 @@ package carpet.mixins;
 
 import carpet.CarpetSettings;
 import net.minecraft.client.renderer.blockentity.BlockEntityWithBoundingBoxRenderer;
-import net.minecraft.world.level.block.entity.StructureBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
