35881baf95f9c05855da65bc2f744a8b970e7d34
Andrew Yong <email.xryong@gmail.com>
2025-12-17 21:08:55 +0800

    fix: remove obsolete injection for starting position in PlayerList_fakePlayersMixin

--- src/main/java/carpet/mixins/PlayerList_fakePlayersMixin.java
+++ src/main/java/carpet/mixins/PlayerList_fakePlayersMixin.java
@@ -1,7 +1,6 @@
 package carpet.mixins;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ClientInformation;
@@ -14,9 +13,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import carpet.patches.NetHandlerPlayServerFake;
 import carpet.patches.EntityPlayerMPFake;
 
@@ -26,19 +23,6 @@ public abstract class PlayerList_fakePlayersMixin
     @Shadow
     @Final
     private MinecraftServer server;
-
-    @Inject(
-            method = "load(Lnet/minecraft/server/level/ServerPlayer;)Lnet/minecraft/nbt/CompoundTag;",
-            at = @At(value = "RETURN", shift = At.Shift.BEFORE),
-            require = 0
-    )
-    private void fixStartingPos(ServerPlayer serverPlayerEntity_1, CallbackInfoReturnable<CompoundTag> cir)
-    {
-        if (serverPlayerEntity_1 instanceof EntityPlayerMPFake)
-        {
-            ((EntityPlayerMPFake) serverPlayerEntity_1).fixStartingPosition.run();
-        }
-    }
 
     @Redirect(method = "placeNewPlayer", at = @At(value = "NEW", target = "(Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/network/Connection;Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/server/network/CommonListenerCookie;)Lnet/minecraft/server/network/ServerGamePacketListenerImpl;"))
     private ServerGamePacketListenerImpl replaceNetworkHandler(MinecraftServer server, Connection clientConnection, ServerPlayer playerIn, CommonListenerCookie cookie)
