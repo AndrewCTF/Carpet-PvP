baaef8971e6a59326161aca42b48566fabfda954
Andrew <email.xryong@gmail.com>
2026-02-11 21:49:41 +0800

    Replace Player_fallDamageRuleMixin with LivingEntity_fallDamageRuleMixin to enhance fall damage handling for all living entities

--- src/main/java/carpet/mixins/LivingEntity_fallDamageRuleMixin.java
+++ src/main/java/carpet/mixins/LivingEntity_fallDamageRuleMixin.java
@@ -2,25 +2,27 @@ package carpet.mixins;
 
 import carpet.CarpetSettings;
 import carpet.patches.EntityPlayerMPFake;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.core.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Player.class)
-public abstract class Player_fallDamageRuleMixin
+@Mixin(LivingEntity.class)
+public abstract class LivingEntity_fallDamageRuleMixin
 {
     @Inject(method = "checkFallDamage", at = @At("HEAD"), cancellable = true)
     private void carpetPvp_gateFallDamage(double y, boolean onGround, BlockState state, BlockPos pos, CallbackInfo ci)
     {
-        if ((Object) this instanceof EntityPlayerMPFake)
+        Object self = this;
+        if (self instanceof EntityPlayerMPFake)
         {
             return;
         }
-        if (!CarpetSettings.playerFallDamage)
+        if (self instanceof Player && !CarpetSettings.playerFallDamage)
         {
             ci.cancel();
         }
