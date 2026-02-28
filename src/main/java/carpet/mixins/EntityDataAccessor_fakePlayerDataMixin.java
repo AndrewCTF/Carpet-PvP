2f0cedb1187ffa6e06388787bde8507521425ae3
Andrew <email.xryong@gmail.com>
2026-02-28 12:59:00 +0800

    Refactor setData method to allow modification of fake player entity data and improve error handling

--- src/main/java/carpet/mixins/EntityDataAccessor_fakePlayerDataMixin.java
+++ src/main/java/carpet/mixins/EntityDataAccessor_fakePlayerDataMixin.java
@@ -2,17 +2,18 @@ package carpet.mixins;
 
 import carpet.CarpetSettings;
 import carpet.patches.EntityPlayerMPFake;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.ProblemReporter;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.storage.TagValueInput;
 import net.minecraft.server.commands.data.EntityDataAccessor;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
 
@@ -23,20 +24,28 @@ public class EntityDataAccessor_fakePlayerDataMixin
     @Final
     private Entity entity;
 
-    @Inject(method = "setData", at = @At("HEAD"), cancellable = true)
-    private void carpetPvp_allowFakePlayerData(CompoundTag tag, CallbackInfo ci)
+    @Shadow
+    @Final
+    private static SimpleCommandExceptionType ERROR_NO_PLAYERS;
+
+    /**
+     * @author Carpet PvP
+     * @reason Allow /data command to modify fake player entity data
+     */
+    @Overwrite
+    public void setData(CompoundTag tag) throws CommandSyntaxException
     {
-        if (CarpetSettings.fakePlayerDataModifiable && this.entity instanceof EntityPlayerMPFake)
+        if (this.entity instanceof Player && !(CarpetSettings.fakePlayerDataModifiable && this.entity instanceof EntityPlayerMPFake))
         {
-            UUID uuid = this.entity.getUUID();
-            try (ProblemReporter.ScopedCollector collector = new ProblemReporter.ScopedCollector(
-                    this.entity.problemPath(),
-                    org.slf4j.LoggerFactory.getLogger(EntityDataAccessor.class)))
-            {
-                this.entity.load(TagValueInput.create(collector, this.entity.registryAccess(), tag));
-            }
-            this.entity.setUUID(uuid);
-            ci.cancel();
+            throw ERROR_NO_PLAYERS.create();
         }
+        UUID uuid = this.entity.getUUID();
+        try (ProblemReporter.ScopedCollector collector = new ProblemReporter.ScopedCollector(
+                this.entity.problemPath(),
+                org.slf4j.LoggerFactory.getLogger(EntityDataAccessor.class)))
+        {
+            this.entity.load(TagValueInput.create(collector, this.entity.registryAccess(), tag));
+        }
+        this.entity.setUUID(uuid);
     }
 }
